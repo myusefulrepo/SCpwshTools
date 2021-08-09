511c205906ae93282e3e6ed1a6fbf2b22bbbad38
ScriptingChris <christian@scriptingchris.tech>
2021-08-09 09:53:15 +0200

    update

---
+++ Source/Public/New-PSPasswordV2.ps1
@@ -0,0 +1,63 @@
+function New-PSPasswordV2 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false)][Switch]$SkipUpperCase,
+        [Parameter(Mandatory=$false)][Switch]$SkipLowerCase,
+        [Parameter(Mandatory=$false)][Switch]$SkipNumbers,
+        [Parameter(Mandatory=$false)][Switch]$SkipSymbols,
+        [Parameter(Mandatory=$false)][Int]$PasswordLength = 12,
+        [Parameter(Mandatory=$false)][Int]$NumberOfPasswords = 1,
+        [Parameter(Mandatory=$false)][String]$PasswordsFilePath
+    )
+    
+    begin {
+        $CharArray = New-Object System.Collections.ArrayList
+        $ValidatePass = New-Object System.Collections.ArrayList
+
+        $LowerLetters = New-Object System.Collections.ArrayList; 97..122 | % {$LowerLetters.Add([Char]$_)} | Out-Null
+        $UpperLetters = New-Object System.Collections.ArrayList; 65..90 | % {$UpperLetters.Add([Char]$_)} | Out-Null
+        $Numbers = New-Object System.Collections.ArrayList; 0..9 | % {$Numbers.Add($_.ToString())} | Out-Null
+        $Symbols = New-Object System.Collections.ArrayList; 33..47 | % {$Symbols.Add([Char]$_)} | Out-Null
+
+        if(!$SkipNumbers.IsPresent){$CharArray.Add($Numbers) | Out-Null; $ValidatePass.Add(1) | Out-Null}
+        if(!$SkipLowerCase.IsPresent){$CharArray.Add($LowerLetters) | Out-Null; $ValidatePass.Add(2) | Out-Null}
+        if(!$SkipUpperCase.IsPresent){$CharArray.Add($UpperLetters) | Out-Null; $ValidatePass.Add(3) | Out-Null}
+        if(!$SkipSymbols.IsPresent){$CharArray.Add($Symbols) | Out-Null; $ValidatePass.Add(4) | Out-Null}
+
+        $WorkingSet = $CharArray | % {$_}
+    }
+    
+    process {
+
+        if($PasswordsFilePath -and !(Test-Path $PasswordsFilePath)){
+            New-Item $PasswordsFilePath -ItemType File
+        }
+
+        for($i = 0; $i -le $NumberOfPasswords; $i++){
+            $Password = New-Object System.Collections.ArrayList
+            for($y = 0; $y -le $PasswordLength; $y++){
+                $Character = $WorkingSet | Get-Random
+                $Password.Add($Character) | Out-Null
+            }
+
+            Switch ($ValidatePass){
+                1 {if(!($Password -match '\d')){$PassNotValid = $true}}
+                2 {if(!($Password -cmatch "[a-z]")){$PassNotValid = $true}}
+                3 {if(!($Password -cmatch "[A-Z]")){$PassNotValid = $true}}
+                4 {
+                    $Password | % {if($Symbols -contains $_){$ContainSymbol = $true}}
+                    if($ContainSymbol -eq $false){$PassNotValid = $true}
+                    $ContainSymbol = $false
+                }
+            }
+            if($PassNotValid -eq $true){$i = $i - 1; $PassNotValid = $false; continue}else {
+                $Password = $Password -join ""
+                if($PasswordsFilePath){Add-Content -Path $PasswordsFilePath -Value $Password}else{Return $Password}
+            }
+        }
+    }
+    
+    end {
+        Write-Verbose -Message "Finishing function"
+    }
+}
